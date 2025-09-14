b650cd217bddc67deca9d604cf1744a1bab4a7a0
MeiVinEight <MeiVinEight@outlook.com>
2025-09-14 19:20:05 +0800

    Add lore

--- src/main/java/com/baizeli/eternisstarrysky/Items/AvaritiaSword.java
+++ src/main/java/com/baizeli/eternisstarrysky/Items/AvaritiaSword.java
@@ -2,7 +2,6 @@ package com.baizeli.eternisstarrysky.Items;
 
 import com.baizeli.Sounds;
 import com.baizeli.eternisstarrysky.Util.TextUtils;
-import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.client.player.LocalPlayer;
@@ -110,15 +109,15 @@ public class AvaritiaSword extends SwordItem {
 
     @Override
     public Component getName(ItemStack p_41458_) {
-        return Component.translatable("§4寰宇支配之剑");
+        return Component.literal("§4寰宇支配之剑");
     }
 
     @Override
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
         p_41423_.clear();
         p_41423_.add(getName(p_41421_));
-        p_41423_.add(Component.translatable(""));
-        p_41423_.add(Component.translatable("§7").append(Component.translatable("item.modifiers.mainhand")));
+        p_41423_.add(Component.literal("这把剑似乎是从1.7.10穿越过来的"));
+        p_41423_.add(Component.literal("§7").append(Component.translatable("item.modifiers.mainhand")));
         p_41423_.add(Component.translatable(TextUtils.makeFabulous("infinity ")).append(TextUtils.makeGreen(I18n.get("attribute.name.generic.attack_damage"))));
         p_41423_.add(Component.translatable(TextUtils.makeGreen("1.6 ")).append(TextUtils.makeGreen(I18n.get("attribute.name.generic.attack_speed"))));
     }
