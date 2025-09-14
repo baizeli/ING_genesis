43787052166f2b2591eca04551c947ada1c37884
MeiVinEight <MeiVinEight@outlook.com>
2025-09-14 18:02:05 +0800

    Fix infinity sword true attack

--- src/main/java/com/baizeli/eternisstarrysky/Items/InfinitySword.java
+++ src/main/java/com/baizeli/eternisstarrysky/Items/InfinitySword.java
@@ -1,5 +1,6 @@
 package com.baizeli.eternisstarrysky.Items;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -18,6 +19,8 @@ import java.util.function.Predicate;
 
 public class InfinitySword extends SwordItem
 {
+	private static final double KILL_RADIUS = 5;
+
 	public InfinitySword(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_)
 	{
 		super(p_43269_, p_43270_, p_43271_, p_43272_);
@@ -26,19 +29,14 @@ public class InfinitySword extends SwordItem
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity)
 	{
-		Level level = player.level();
-		if (entity == null || level.isClientSide() || !(entity instanceof LivingEntity))
-			return super.onLeftClickEntity(stack, player, entity);
-		EntityTypeTest<Entity, LivingEntity> typeTest = EntityTypeTest.forClass(LivingEntity.class);
-		Vec3 pos = entity.position();
-		double radius = 5;
-		AABB aabb = new AABB(
-			pos.x - radius, pos.y - radius, pos.z - radius,
-			pos.x + radius, pos.y + radius, pos.z + radius
-		);
-		List<LivingEntity> nearbyEntities = new LinkedList<>();
+		if (!(player instanceof ServerPlayer))
+			return false;
+
+		if (!(entity instanceof LivingEntity))
+			return false;
+
 		Predicate<LivingEntity> predicate = (e) -> e.getId() != player.getId();
-		level.getEntities(typeTest, aabb, predicate, nearbyEntities, Integer.MAX_VALUE);
+		List<LivingEntity> nearbyEntities = InfinitySword.getNearbyLivingEntities(entity, KILL_RADIUS, predicate);
 
 		DamageSource source = new DamageSource(player.damageSources().fellOutOfWorld().typeHolder(), player);
 		nearbyEntities.forEach(living -> {
@@ -46,5 +44,24 @@ public class InfinitySword extends SwordItem
 			living.hurt(source, this.getDamage());
 		});
 		return true;
+	}
+
+	public static List<LivingEntity> getNearbyLivingEntities(Entity entity, double radius, Predicate<LivingEntity> predicate)
+	{
+		Level level;
+		if (entity == null || (level = entity.level()).isClientSide)
+			return List.of();
+
+		EntityTypeTest<Entity, LivingEntity> typeTest = EntityTypeTest.forClass(LivingEntity.class);
+		Vec3 pos = entity.position();
+		AABB aabb = new AABB(
+			pos.x - radius, pos.y - radius, pos.z - radius,
+			pos.x + radius, pos.y + radius, pos.z + radius
+		);
+		List<LivingEntity> nearbyEntities = new LinkedList<>();
+		if (predicate == null)
+			predicate = (e) -> true;
+		level.getEntities(typeTest, aabb, predicate, nearbyEntities, Integer.MAX_VALUE);
+		return nearbyEntities;
 	}
 }
--- src/main/java/com/baizeli/eternisstarrysky/Items/InfinitySwordTrue.java
+++ src/main/java/com/baizeli/eternisstarrysky/Items/InfinitySwordTrue.java
@@ -6,29 +6,35 @@ import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.font.FontSet;
-import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.FormattedCharSequence;
-import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageSources;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.ExperienceOrb;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.entity.decoration.ItemFrame;
 import net.minecraft.world.entity.decoration.Painting;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -37,19 +43,19 @@ import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static com.baizeli.eternisstarrysky.RainbowEffectHelper.BLUE;
 
 public final class InfinitySwordTrue extends SwordItem
 {
+	private static final double KILL_RADIUS = 5;
 
 	//归零的数据
-	public static List datas = new ArrayList();
+	// public static List datas = new ArrayList();
 
 	public InfinitySwordTrue(Tier tier, int attackDamage, float attackSpeed, Properties properties)
 	{
@@ -271,63 +277,20 @@ public final class InfinitySwordTrue extends SwordItem
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity)
 	{
+		if (!(player instanceof ServerPlayer))
+			return false;
 
-		if (entity instanceof LivingEntity target)
-		{
-			target.removeAllEffects();
-			target.invulnerableTime = 0;
-			target.hurtTime = 0;
-		}
+		if (!(entity instanceof LivingEntity))
+			return false;
 
-		if (entity != null)
-		{
-			if (entity instanceof Player targetPlayer && targetPlayer.isCreative())
-			{
-				return false;
-			}
-
-			if (entity instanceof LivingEntity livingEntity)
-			{
-				Level level = livingEntity.level();
-				Vec3 playerPos = player.position();
-				double radius = 5.0;
-
-				AABB searchArea = new AABB(
-						playerPos.x - radius, playerPos.y - radius, playerPos.z - radius,
-						playerPos.x + radius, playerPos.y + radius, playerPos.z + radius
-				);
-				List<LivingEntity> nearbyEntities = level.getEntitiesOfClass(LivingEntity.class, searchArea,
-						entityi -> entityi != null
-								&& entityi != player
-								&& entityi.distanceTo(player) <= radius
-								&& !(entityi instanceof Player));
-
-				livingEntity.removeAllEffects();
-				livingEntity.invulnerableTime = 0;
-				livingEntity.hurtTime = 0;
-				DamageSource void_damage = new DamageSource(entity.damageSources().genericKill().typeHolder(),player);
-				livingEntity.hurt(void_damage, Float.MAX_VALUE);
-				 livingEntity.setHealth(0);
-				datas.add(entity.getEntityData());
-
-				Random random = new Random();
-				nearbyEntities.forEach(living->{
-					player.crit(living);
-					datas.add(living.getEntityData());
-				});
-				//LightningBolt lightning = new LightningBolt(EntityType.LIGHTNING_BOLT, level);
-				//lightning.setVisualOnly(true);
-				//lightning.moveTo(entity.position().x, entity.position().y, entity.position().z);
-
-				//int randomId = random.nextInt(10001) + 20000;
-				//lightning.setId(200000000 + randomId);
-
-				//level.addFreshEntity(lightning);
-			}
-
-			return true;
-		}
-		return super.onLeftClickEntity(stack, player, null);
+		DamageSource voidDamage = new DamageSource(player.damageSources().genericKill().typeHolder(), player);
+		Predicate<LivingEntity> predicate = (e) -> e.getId() != player.getId();
+		List<LivingEntity> nearbyEntities = InfinitySword.getNearbyLivingEntities(entity, KILL_RADIUS, predicate);
+		nearbyEntities.forEach(e -> {
+			player.crit(e);
+			e.hurt(voidDamage, Float.MAX_VALUE);
+		});
+		return true;
 	}
 
 	@Override
