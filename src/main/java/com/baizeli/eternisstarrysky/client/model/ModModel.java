5ed4d4e24587f0d792eaf672044fd7fcf7d69428
Xiaoyu_2009 <3551371733@qq.com>
2025-12-01 01:15:07 +0800

    添加了死星天裁-[星源]法术卷轴陨石相关模型替换原有的陨石渲染器/并替换已有对应纹理换成单一贴图/并新建了模型主类/并调整了些许渲染器大小迎合碰撞箱

---
+++ src/main/java/com/baizeli/eternisstarrysky/client/model/ModModel.java
@@ -0,0 +1,16 @@
+package com.baizeli.eternisstarrysky.client.model;
+
+import com.baizeli.eternisstarrysky.EternisStarrySky;
+import com.baizeli.eternisstarrysky.client.model.spell.celestial_source.*;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = EternisStarrySky.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModModel {
+    
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(DeadStarDecreeCometModel.LAYER_LOCATION, DeadStarDecreeCometModel::createBodyLayer);
+    }
+}
