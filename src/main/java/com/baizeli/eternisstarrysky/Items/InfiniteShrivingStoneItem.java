a9abd0a2c5d39d628cbe5eea8ff5c7e544b36958
Xiaoyu_2009 <3551371733@qq.com>
2025-12-04 01:32:10 +0800

    完善了无限忏悔石/修复了些许问题

--- src/main/java/com/baizeli/eternisstarrysky/Items/InfiniteShrivingStoneItem.java
+++ src/main/java/com/baizeli/eternisstarrysky/Items/InfiniteShrivingStoneItem.java
@@ -1,21 +1,5 @@
 package com.baizeli.eternisstarrysky.Items;
 
-import io.redspace.ironsspellbooks.util.ItemPropertiesHelper;
-import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
+import io.redspace.ironsspellbooks.item.ShrivingStoneItem;
 
-import java.util.*;
-
-public class InfiniteShrivingStoneItem extends Item {
-    public InfiniteShrivingStoneItem() {
-        super(ItemPropertiesHelper.material());
-    }
-
-    @Override
-    public void appendHoverText(ItemStack pStack, Level context, List<Component> lines, TooltipFlag pIsAdvanced) {
-        super.appendHoverText(pStack, context, lines, pIsAdvanced);
-        lines.add(Component.translatable("item.irons_spellbooks.shriving_stone_desc").withStyle(ChatFormatting.GRAY));
-    }
-}
+public class InfiniteShrivingStoneItem extends ShrivingStoneItem {}
