a2122a10669bab1f5effa5de6684e1aca3908452
Xiaoyu_2009 <3551371733@qq.com>
2025-12-14 19:19:02 +0800

    增加了血之狂乱-[混沌]法术卷轴身处于buff的特殊视觉效果/改动了些许Mixin

--- src/main/java/com/baizeli/eternisstarrysky/Mixin/FoodDataMixin.java
+++ src/main/java/com/baizeli/eternisstarrysky/Mixin/FoodDataMixin.java
@@ -3,10 +3,8 @@ package com.baizeli.eternisstarrysky.Mixin;
 import com.baizeli.eternisstarrysky.effect.spell.ModEffect;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FoodData.class)
--- src/main/java/com/baizeli/eternisstarrysky/Mixin/minecraft/client/renderer/GameRendererAccessor.java
+++ src/main/java/com/baizeli/eternisstarrysky/Mixin/minecraft/client/renderer/GameRendererAccessor.java
@@ -1,4 +1,4 @@
-package com.baizeli.eternisstarrysky.Mixin;
+package com.baizeli.eternisstarrysky.Mixin.minecraft.client.renderer;
 
 import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.GameRenderer;
