a6d8a7a9623bec266f735c3142dc8e72aeeea879
Xiaoyu_2009 <3551371733@qq.com>
2025-12-21 00:29:08 +0800

    修复了我要飞的一些问题

--- src/main/java/com/baizeli/eternisstarrysky/effect/spell/celestial_source/IFlyEffect.java
+++ src/main/java/com/baizeli/eternisstarrysky/effect/spell/celestial_source/IFlyEffect.java
@@ -2,9 +2,6 @@ package com.baizeli.eternisstarrysky.effect.spell.celestial_source;
 
 import com.baizeli.eternisstarrysky.EternisStarrySky;
 import net.minecraft.world.effect.*;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeMap;
-import net.minecraft.world.entity.player.Player;
 
 public class IFlyEffect extends MobEffect {
     public IFlyEffect() {
@@ -14,31 +11,5 @@ public class IFlyEffect extends MobEffect {
     @Override
     public String getDescriptionId() {
         return "effect." + EternisStarrySky.MOD_ID + ".i_fly";
-    }
-    
-    @Override
-    public void applyEffectTick(LivingEntity entity, int amplifier) {
-        if (entity instanceof Player player) {
-            player.getAbilities().mayfly = true;
-            player.onUpdateAbilities();
-        }
-
-        super.applyEffectTick(entity, amplifier);
-    }
-    
-    @Override
-    public boolean isDurationEffectTick(int duration, int amplifier) {
-        return true;
-    }
-    
-    @Override
-    public void removeAttributeModifiers(LivingEntity entity, AttributeMap attributeMap, int amplifier) {
-        if (entity instanceof Player player) {
-            player.getAbilities().mayfly = false;
-            player.getAbilities().flying = false;
-            player.onUpdateAbilities();
-        }
-
-        super.removeAttributeModifiers(entity, attributeMap, amplifier);
     }
 }
