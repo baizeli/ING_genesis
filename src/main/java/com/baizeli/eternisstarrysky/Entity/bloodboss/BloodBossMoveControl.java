c91e242b54974c5b43f21b260302f82f13a2527e
MeiVinEight <MeiVinEight@outlook.com>
2026-01-14 12:01:28 +0800

    混沌受血者-移动控制器

---
+++ src/main/java/com/baizeli/eternisstarrysky/Entity/bloodboss/BloodBossMoveControl.java
@@ -0,0 +1,11 @@
+package com.baizeli.eternisstarrysky.Entity.bloodboss;
+
+import net.minecraft.world.entity.ai.control.MoveControl;
+
+public class BloodBossMoveControl extends MoveControl
+{
+	public BloodBossMoveControl(BloodBossEntity mob)
+	{
+		super(mob);
+	}
+}
