c2ec3189314368dc0665fc09c254340d69a32144
V_BlackCAT <2049304687@qq.com>
2026-01-21 01:33:57 +0800

    refactor(boss): BloodBoss模型渲染重构为法术施放实体基类
    - 继承AbstractSpellCastingMobModel和AbstractSpellCastingMobRenderer基类
    - 添加铁spell书mod的渲染层支持包括充能法术层、能量漩涡层等
    - 创建MixinChargeSpellLayerGeo来重定向骨骼绑定到自定义骨骼
    - 移动PostChainAccessor到minecraft客户端渲染包路径下
    - 更新mixin配置文件中的包路径引用

--- src/main/java/miku/united_as_one/genesis/client/model/boss/BloodBossModel.java
+++ src/main/java/miku/united_as_one/genesis/client/model/boss/BloodBossModel.java
@@ -1,25 +1,30 @@
 package miku.united_as_one.genesis.client.model.boss;
 
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobModel;
 import miku.united_as_one.genesis.Genesis;
 import miku.united_as_one.genesis.entity.boss.BloodBoss;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
 
 @SuppressWarnings("removal")
-public class BloodBossModel extends GeoModel<BloodBoss> {
+public class BloodBossModel extends AbstractSpellCastingMobModel {
 
-    @Override
-    public ResourceLocation getModelResource(BloodBoss animatable) {
+
+    public ResourceLocation getModelResource(AbstractSpellCastingMob object) {
         return new ResourceLocation(Genesis.MOD_ID, "geo/entity/blood_boss.geo.json");
     }
 
+
     @Override
-    public ResourceLocation getTextureResource(BloodBoss animatable) {
+    public ResourceLocation getTextureResource(AbstractSpellCastingMob abstractSpellCastingMob) {
         return new ResourceLocation(Genesis.MOD_ID, "textures/entity/blood_boss/stage_1.png");
     }
 
     @Override
-    public ResourceLocation getAnimationResource(BloodBoss animatable) {
-        return new ResourceLocation(Genesis.MOD_ID, "animations/entity/blood_boss.animation.json");
+    public void setCustomAnimations(AbstractSpellCastingMob entity, long instanceId,
+                                    AnimationState<AbstractSpellCastingMob> animationState) {
+        super.setCustomAnimations(entity, instanceId, animationState);
     }
 }
--- src/main/java/miku/united_as_one/genesis/client/renderer/boss/BloodBossRenderer.java
+++ src/main/java/miku/united_as_one/genesis/client/renderer/boss/BloodBossRenderer.java
@@ -1,12 +1,26 @@
 package miku.united_as_one.genesis.client.renderer.boss;
 
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobRenderer;
+import io.redspace.ironsspellbooks.render.ChargeSpellLayer;
+import io.redspace.ironsspellbooks.render.EnergySwirlLayer;
+import io.redspace.ironsspellbooks.render.GlowingEyesLayer;
+import io.redspace.ironsspellbooks.render.SpellTargetingLayer;
 import miku.united_as_one.genesis.entity.boss.BloodBoss;
 import miku.united_as_one.genesis.client.model.boss.BloodBossModel;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
+import software.bernie.geckolib.renderer.layer.GeoRenderLayersContainer;
 
-public class BloodBossRenderer extends GeoEntityRenderer<BloodBoss> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BloodBossRenderer extends AbstractSpellCastingMobRenderer{
+
     public BloodBossRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new BloodBossModel());
     }
+
+
+
 }
