c3cce9645fd4fd32cc7116c572cce968ff24483e
V_BlackCAT <2049304687@qq.com>
2026-01-21 19:40:54 +0800

    注册受血者AI

---
+++ src/main/java/miku/united_as_one/genesis/Entity/ai/ModActivity.java
@@ -0,0 +1,22 @@
+package miku.united_as_one.genesis.entity.ai;
+
+import net.minecraft.world.entity.schedule.Activity;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static miku.united_as_one.genesis.Genesis.MODID;
+
+
+public class ModActivity {
+    public static final DeferredRegister<Activity> MOD_ACTIVITY =
+            DeferredRegister.create(ForgeRegistries.Keys.ACTIVITIES, MODID);
+
+    public static final RegistryObject<Activity> TEST_ACTIVITY =
+            MOD_ACTIVITY.register("test_activity", () -> new Activity("test_activity"));
+
+    public static void register(IEventBus modEventBus) {
+        MOD_ACTIVITY.register(modEventBus);
+    }
+}
