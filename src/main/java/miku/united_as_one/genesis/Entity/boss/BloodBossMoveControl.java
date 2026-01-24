a9ccec7ce6dc0c505613332fcfb86d00d30b4b1a
V_BlackCAT <2049304687@qq.com>
2026-01-24 20:35:52 +0800

    继续做受血者

---
+++ src/main/java/miku/united_as_one/genesis/Entity/boss/BloodBossMoveControl.java
@@ -0,0 +1,12 @@
+package miku.united_as_one.genesis.entity.boss;
+
+import net.minecraft.world.entity.ai.control.MoveControl;
+
+public class BloodBossMoveControl extends MoveControl
+{
+    public BloodBossMoveControl(BloodBoss mob)
+    {
+        super(mob);
+    }
+}
+
