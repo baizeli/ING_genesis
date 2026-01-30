4124cd61fd84153ef0acec7246edfc83647397c1
Xiaoyu_2009 <3551371733@qq.com>
2026-01-30 19:38:51 +0800

    逐渐完善巫术弓

--- src/main/java/miku/united_as_one/genesis/items/bow/WitchcraftBow.java
+++ src/main/java/miku/united_as_one/genesis/items/bow/WitchcraftBow.java
@@ -6,9 +6,11 @@ import io.redspace.ironsspellbooks.api.events.SpellOnCastEvent;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.damage.DamageSources;
+import io.redspace.ironsspellbooks.entity.spells.eldritch_blast.EldritchBlastVisualEntity;
 import io.redspace.ironsspellbooks.network.SyncManaPacket;
 import io.redspace.ironsspellbooks.setup.PacketDistributor;
 import io.redspace.ironsspellbooks.api.util.*;
+import io.redspace.ironsspellbooks.spells.eldritch.EldritchBlastSpell;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.*;
@@ -51,13 +53,37 @@ public class WitchcraftBow extends BowItem {
         float range = 22;
         Vec3 start = player.getEyePosition();
         Vec3 end = start.add(player.getForward().scale(range));
-        
+
         for (Entity target : level.getEntities(player, player.getBoundingBox().expandTowards(end.subtract(start)))) {
             if (Utils.checkEntityIntersecting(target, start, end, .4f).getType() != HitResult.Type.MISS) {
                 DamageSources.applyDamage(target, power * 16, level.damageSources().sonicBoom(player));
             }
         }
-        
+
+        Vec3 playerRight = player.getLookAngle();
+        Vec3 leftStart = start.add(playerRight.scale(0)).add(new Vec3(0, -0.5, 0));
+        /*Vec3 rightStart = start.add(playerRight.scale(0)).add(new Vec3(0, -0.5, 0));*/
+
+        // 左侧巫术湮灭射线
+        Vec3 leftEnd = leftStart.add(player.getForward().scale(range));
+        level.addFreshEntity(new EldritchBlastVisualEntity(level, leftStart, leftEnd, player));
+
+        /*for (Entity target : level.getEntities(player, player.getBoundingBox().expandTowards(leftEnd.subtract(leftStart)))) {
+            if (Utils.checkEntityIntersecting(target, leftStart, leftEnd, .4f).getType() != HitResult.Type.MISS) {
+                DamageSources.applyDamage(target, power * 8, new EldritchBlastSpell().getDamageSource(target, player));
+            }
+        }*/
+
+        // 右侧巫术湮灭射线
+        /*Vec3 rightEnd = rightStart.add(player.getForward().scale(range));
+        level.addFreshEntity(new EldritchBlastVisualEntity(level, rightStart, rightEnd, player));
+
+        for (Entity target : level.getEntities(player, player.getBoundingBox().expandTowards(rightEnd.subtract(rightStart)))) {
+            if (Utils.checkEntityIntersecting(target, rightStart, rightEnd, .4f).getType() != HitResult.Type.MISS) {
+                DamageSources.applyDamage(target, power * 8, new EldritchBlastSpell().getDamageSource(target, player));
+            }
+        }*/
+
         level.playSound(
             null, player.getX(), player.getY(), player.getZ(), 
             SoundEvents.WARDEN_SONIC_BOOM, 
