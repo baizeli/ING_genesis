0b25ea739aceb422a487901c92d109215aa164d3
Xiaoyu_2009 <3551371733@qq.com>
2026-01-31 21:55:48 +0800

    添加了烈焰法术穿透[属性修饰符]

--- src/main/java/miku/united_as_one/genesis/event/spell/SpellAttributesEvent.java
+++ src/main/java/miku/united_as_one/genesis/event/spell/SpellAttributesEvent.java
@@ -7,7 +7,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Genesis.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class SpellAttributeEvent {
+public class SpellAttributesEvent {
 
     @SubscribeEvent
     public static void modifyEntityAttributes(EntityAttributeModificationEvent e) {
---
+++ src/main/java/miku/united_as_one/genesis/event/spell/attributes/FlameSpellPenetrationEvent.java
@@ -0,0 +1,27 @@
+package miku.united_as_one.genesis.event.spell.attributes;
+
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.events.SpellDamageEvent;
+import miku.united_as_one.genesis.registry.spell.SpellAttributesRegistry;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class FlameSpellPenetrationEvent {
+
+    @SubscribeEvent
+    public static void onSpellDamage(SpellDamageEvent event) {
+        var spellDamageSource = event.getSpellDamageSource();
+        
+        if (spellDamageSource.spell().getSchoolType() == SchoolRegistry.FIRE.get()) {
+            if (spellDamageSource.getEntity() instanceof LivingEntity livingAttacker) {
+                double flamePenetration = livingAttacker.getAttributeValue(SpellAttributesRegistry.FLAME_SPELL_PENETRATION.get());
+
+                if (flamePenetration > 0) {
+                    event.setAmount(event.getAmount() * (1 + (float) flamePenetration));
+                }
+            }
+        }
+    }
+}
