9890f2f8c7a22db16e53946170962e8e9a0a7fb5
Xiaoyu_2009 <3551371733@qq.com>
2026-01-31 21:35:01 +0800

    修复了属性修饰符已知的问题并添加了新的属性修饰符烈焰法术穿透/修复已知问题

---
+++ src/main/java/miku/united_as_one/genesis/event/spell/SpellAttributeEvent.java
@@ -0,0 +1,21 @@
+package miku.united_as_one.genesis.event.spell;
+
+import miku.united_as_one.genesis.Genesis;
+import miku.united_as_one.genesis.registry.spell.SpellAttributesRegistry;
+import net.minecraftforge.event.entity.EntityAttributeModificationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Genesis.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SpellAttributeEvent {
+
+    @SubscribeEvent
+    public static void modifyEntityAttributes(EntityAttributeModificationEvent e) {
+        e.getTypes().forEach(
+            entity -> SpellAttributesRegistry
+                .getAttributes()
+                .getEntries()
+                .forEach(attribute -> e.add(entity, attribute.get()))
+        );
+    }
+}
--- src/main/java/miku/united_as_one/genesis/registry/spell/SpellAttributesRegistry.java
+++ src/main/java/miku/united_as_one/genesis/registry/spell/SpellAttributesRegistry.java
@@ -14,6 +14,7 @@ public class SpellAttributesRegistry {
     public static final RegistryObject<Attribute> CELESTIAL_SOURCE_SPELL_POWER;
     public static final RegistryObject<Attribute> CULINARY_MAGIC_RESIST;
     public static final RegistryObject<Attribute> CULINARY_SPELL_POWER;
+    public static final RegistryObject<Attribute> FLAME_SPELL_PENETRATION;
 
     static {
         ATTRIBUTES = DeferredRegister.create(ForgeRegistries.ATTRIBUTES, Genesis.MOD_ID);
@@ -28,10 +29,17 @@ public class SpellAttributesRegistry {
         // 美食法术强度/抗性
         CULINARY_SPELL_POWER = SpellAttributesRegistry.newPowerAttribute("culinary");
         CULINARY_MAGIC_RESIST = SpellAttributesRegistry.newResistanceAttribute("culinary");
+        
+        // 烈焰法术穿透
+        FLAME_SPELL_PENETRATION = SpellAttributesRegistry.newPenetrationAttribute();
     }
 
     public static void register(IEventBus eventBus) {
         ATTRIBUTES.register(eventBus);
+    }
+
+    public static DeferredRegister<Attribute> getAttributes() {
+        return ATTRIBUTES;
     }
 
     private static RegistryObject<Attribute> newResistanceAttribute(String id) {
@@ -40,5 +48,13 @@ public class SpellAttributesRegistry {
 
     private static RegistryObject<Attribute> newPowerAttribute(String id) {
         return ATTRIBUTES.register(id + "_spell_power", () -> (new MagicPercentAttribute("attribute." + Genesis.MOD_ID + "." + id + "_spell_power", 1.0F, Double.MIN_VALUE, Double.MAX_VALUE)).setSyncable(true));
+    }
+
+    // 烈焰法术穿透
+    private static RegistryObject<Attribute> newPenetrationAttribute() {
+        return ATTRIBUTES.register("flame_spell_penetration", () -> (new MagicPercentAttribute(
+            "attribute." + Genesis.MOD_ID + ".flame_spell_penetration",
+            1, -Double.MIN_VALUE, Double.MAX_VALUE)).setSyncable(true)
+        );
     }
 }
