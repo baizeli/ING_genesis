7d6109f4776bcb7c0643f3a4e16d12ec0af0f44f
bai_ze_li <631557194@qq.com>
2026-02-10 15:32:03 +0800

    注册物品

--- src/main/java/miku/united_as_one/genesis/common/spell/UpgradeOrbTypes.java
+++ src/main/java/miku/united_as_one/genesis/common/spell/UpgradeOrbTypes.java
@@ -9,17 +9,17 @@ import net.minecraft.resources.ResourceKey;
 public class UpgradeOrbTypes {
     public static final ResourceKey<Registry<UpgradeOrbType>> KEY = UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY;
 
-    //混沌法强
+    //混沌法强+1
     public static final ResourceKey<UpgradeOrbType> CHAOS_SPELL_POWER = ResourceKey.create(
         KEY, Genesis.rl("chaos_power")
     );
 
-    //烈焰穿透
+    //烈焰穿透+1
     public static final ResourceKey<UpgradeOrbType> FLAME_SPELL_PENETRATION = ResourceKey.create(
         KEY, Genesis.rl("flame_spell_penetration")
     );
 
-    //星源法强
+    //星源法强+1
     public static final ResourceKey<UpgradeOrbType> CELESTIAL_SOURCE_SPELL_POWER = ResourceKey.create(
         KEY, Genesis.rl("celestial_source_power")
     );
@@ -28,42 +28,42 @@ public class UpgradeOrbTypes {
         KEY, Genesis.rl("culinary_power")
     );
 
-    //远古巫术
+    //远古巫术+1
     public static final ResourceKey<UpgradeOrbType> ELDRITCH_SPELL_POWER = ResourceKey.create(
         KEY, Genesis.rl("eldritch_power")
     );
 
-    //神圣穿透
+    //神圣穿透+1
     public static final ResourceKey<UpgradeOrbType> HOLY_SPELL_PENETRATION = ResourceKey.create(
         KEY,Genesis.rl("holy_spell_penetration")
     );
 
-   //傻逼冰霜
+   //傻逼冰霜+1
     public static final ResourceKey<UpgradeOrbType> FROST_SPELL_PENETRATION = ResourceKey.create(
         KEY,Genesis.rl("frost_spell_penetration")
     );
 
-    //猩红穿透
+    //猩红穿透+1
     public static final ResourceKey<UpgradeOrbType> SCARLET_SPELL_PENETRATION = ResourceKey.create(
         KEY,Genesis.rl("scarlet_spell_penetration")
     );
 
-    //末影穿透
+    //末影穿透+1
     public static final ResourceKey<UpgradeOrbType> ENDER_SPELL_PENETRATION = ResourceKey.create(
             KEY,Genesis.rl("ender_spell_penetration")
     );
 
-    //雷霆穿透
+    //雷霆穿透+1
     public static final ResourceKey<UpgradeOrbType> THUNDER_SPELL_PENETRATION = ResourceKey.create(
             KEY,Genesis.rl("thunder_spell_penetration")
     );
 
-    //自然穿透
+    //自然穿透+1
     public static final ResourceKey<UpgradeOrbType> NATURE_SPELL_PENETRATION = ResourceKey.create(
             KEY,Genesis.rl("nature_spell_penetration")
     );
 
-    //邪术穿透
+    //邪术穿透+1
     public static final ResourceKey<UpgradeOrbType> WARLOCK_SPELL_PENETRATION = ResourceKey.create(
             KEY,Genesis.rl("warlock_spell_penetration")
     );
