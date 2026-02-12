bcaa8c99bd4540005a7d478e2e47f2ebc42c4c47
白仄黎 <16283760+bai-zhe-li@user.noreply.gitee.com>
2026-02-12 23:44:07 +0800

    111

--- src/main/java/miku/united_as_one/genesis/init/registry/BlockRegistry.java
+++ src/main/java/miku/united_as_one/genesis/init/registry/BlockRegistry.java
@@ -1,10 +1,12 @@
 package miku.united_as_one.genesis.init.registry;
 
+import com.tterrag.registrate.util.entry.ItemEntry;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import miku.united_as_one.genesis.common.block.ChaosPortalBlock;
 import miku.united_as_one.genesis.common.data.content.arcaneWorkbench.ArcaneWorkbenchBlock;
 import miku.united_as_one.genesis.Genesis;
 import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.*;
@@ -12,6 +14,31 @@ import net.minecraftforge.registries.*;
 
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Genesis.MOD_ID);
+
+    //风化砂岩
+    public static final RegistryObject<Block> WEATHERED_SANDSTONE;
+    public static final RegistryObject<StairBlock> WEATHERED_SANDSTONE_STAIRS;
+    public static final RegistryObject<SlabBlock> WEATHERED_SANDSTONE_SLAB;
+    public static final RegistryObject<WallBlock> WEATHERED_SANDSTONE_WALL;
+
+    static {
+        WEATHERED_SANDSTONE = BLOCKS.register("weathered_sandstone", () -> new Block(
+                BlockBehaviour.Properties.of()
+        ));
+
+        WEATHERED_SANDSTONE_STAIRS = BLOCKS.register("weathered_sandstone_stairs", () -> new StairBlock(
+                () -> WEATHERED_SANDSTONE.get().defaultBlockState(),
+                BlockBehaviour.Properties.of()
+        ));
+
+        WEATHERED_SANDSTONE_SLAB = BLOCKS.register("weathered_sandstone_slab", () -> new SlabBlock(
+                BlockBehaviour.Properties.of()
+        ));
+
+        WEATHERED_SANDSTONE_WALL = BLOCKS.register("weathered_sandstone_wall", () -> new WallBlock(
+                BlockBehaviour.Properties.of()
+        ));
+    }
 
     // 星源块
     public static final RegistryEntry<Block> CELESTIAL_SOURCE_BLOCK = Genesis.L2_REGISTRATE
