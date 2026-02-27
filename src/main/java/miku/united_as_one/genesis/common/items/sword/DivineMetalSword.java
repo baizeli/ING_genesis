f7112b019aed58d77c6bf47652b22a46f9acd61c
Xiaoyu_2009 <3551371733@qq.com>
2026-02-28 05:15:00 +0800

    咕咕嘎嘎的测试

--- src/main/java/miku/united_as_one/genesis/common/items/sword/DivineMetalSword.java
+++ src/main/java/miku/united_as_one/genesis/common/items/sword/DivineMetalSword.java
@@ -1,6 +1,8 @@
 package miku.united_as_one.genesis.common.items.sword;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.*;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.*;
@@ -10,6 +12,19 @@ import java.util.List;
 public class DivineMetalSword extends SwordItem {
     public DivineMetalSword(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
         super(tier, attackDamage, attackSpeed, properties);
+    }
+
+    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack pStack) {
+        return UseAnim.BLOCK;
+    }
+
+    public int getUseDuration(@NotNull ItemStack pStack) {
+        return Integer.MAX_VALUE;
+    }
+
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
+        player.startUsingItem(hand);
+        return InteractionResultHolder.consume(player.getItemInHand(hand));
     }
 
     public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
