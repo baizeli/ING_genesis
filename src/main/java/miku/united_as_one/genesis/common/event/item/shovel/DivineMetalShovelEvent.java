b8d30e105384ecb92f2a05a3eea3bb4e380e23e4
Xiaoyu_2009 <3551371733@qq.com>
2026-02-27 21:11:29 +0800

    实现了神圣金属铲的逻辑

--- src/main/java/miku/united_as_one/genesis/common/event/item/shovel/DivineMetalShovelEvent.java
+++ src/main/java/miku/united_as_one/genesis/common/event/item/shovel/DivineMetalShovelEvent.java
@@ -1,5 +1,31 @@
 package miku.united_as_one.genesis.common.event.item.shovel;
 
+import miku.united_as_one.genesis.common.items.shovel.DivineMetalShovel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
 public class DivineMetalShovelEvent {
     
+    @SubscribeEvent
+    public static void onBlockBreak(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        Level level = (Level) event.getLevel();
+        BlockPos pos = event.getPos();
+        BlockState state = event.getState();
+
+        if (player.getMainHandItem().getItem() instanceof DivineMetalShovel && player.isShiftKeyDown()) {
+            if (state.getBlock() == Blocks.SAND) {
+                level.destroyBlock(pos, false);
+                Block.popResource(level, pos, new ItemStack(Blocks.GLASS));
+            }
+        }
+    }
 }
