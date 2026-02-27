8e708ccd794898b20e3c632064cd5817183c285f
Xiaoyu_2009 <3551371733@qq.com>
2026-02-28 05:40:58 +0800

    咕咕嘎嘎测试

--- src/main/java/miku/united_as_one/genesis/common/items/sword/MithrilSword.java
+++ src/main/java/miku/united_as_one/genesis/common/items/sword/MithrilSword.java
@@ -2,11 +2,7 @@ package miku.united_as_one.genesis.common.items.sword;
 
 import io.redspace.ironsspellbooks.api.item.weapons.MagicSwordItem;
 import io.redspace.ironsspellbooks.api.registry.*;
-import net.minecraft.world.*;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.*;
 
 import java.util.Map;
 
@@ -14,18 +10,5 @@ import java.util.Map;
 public class MithrilSword extends MagicSwordItem {
     public MithrilSword(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
         super(tier, attackDamage, attackSpeed, SpellDataRegistryHolder.of(new SpellDataRegistryHolder(SpellRegistry.RAY_OF_FROST_SPELL, 5)), Map.of(), properties);
-    }
-
-    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack pStack) {
-        return UseAnim.BLOCK;
-    }
-
-    public int getUseDuration(@NotNull ItemStack pStack) {
-        return Integer.MAX_VALUE;
-    }
-
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
-        player.startUsingItem(hand);
-        return InteractionResultHolder.consume(player.getItemInHand(hand));
     }
 }
