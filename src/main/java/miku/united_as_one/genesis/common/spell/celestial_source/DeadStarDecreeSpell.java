5ebc86f225ce29db82615cb33396100e4c4d4bc5
Xiaoyu_2009 <3551371733@qq.com>
2026-03-03 03:50:39 +0800

    死星天裁，冷却50s，法力消耗100/200/300，效果半径为10/11/12，伤害砍75%(大陨石和小陨石)

--- src/main/java/miku/united_as_one/genesis/common/spell/celestial_source/DeadStarDecreeSpell.java
+++ src/main/java/miku/united_as_one/genesis/common/spell/celestial_source/DeadStarDecreeSpell.java
@@ -32,15 +32,15 @@ public class DeadStarDecreeSpell extends AbstractSpell {
         .setMinRarity(SpellRarity.COMMON)
         .setSchoolResource(SpellSchoolRegistry.CELESTIAL_SOURCE_RESOURCE)
         .setMaxLevel(3)
-        .setCooldownSeconds(240)
+        .setCooldownSeconds(50)
         .build();
 
     public DeadStarDecreeSpell() {
-        this.manaCostPerLevel = 50;
+        this.manaCostPerLevel = 100;
         this.baseSpellPower = 25;
         this.spellPowerPerLevel = 25;
         this.castTime = 60;
-        this.baseManaCost = 500;
+        this.baseManaCost = 100;
     }
 
     @Override
@@ -74,16 +74,19 @@ public class DeadStarDecreeSpell extends AbstractSpell {
 
     // 陨石的伤害
     private float getDamage(int spellLevel, LivingEntity caster) {
-        return 10 * spellLevel + getSpellPower(spellLevel, caster) - 1.0f;
+        float originalDamage = 10 * spellLevel + getSpellPower(spellLevel, caster) - 1;
+        return originalDamage * 0.25f;
     }
     
     // 大陨石的伤害
     private float getLargeCometDamage(int spellLevel, LivingEntity caster) {
-        return 100 + (spellLevel - 1) * 10 + getSpellPower(spellLevel, caster) - 1;
+        float originalDamage = 100 + (spellLevel - 1) * 10 + getSpellPower(spellLevel, caster) - 1;
+        return originalDamage * 0.25f;
     }
 
     private float getRadius(int spellLevel, LivingEntity caster) {
-        return 15;
+        float[] radii = {10.0f, 11.0f, 12.0f};
+        return radii[Math.min(spellLevel - 1, radii.length - 1)];
     }
 
     @Override
@@ -93,7 +96,8 @@ public class DeadStarDecreeSpell extends AbstractSpell {
 
     @Override
     public int getManaCost(int spellLevel) {
-        return 500 + (spellLevel - 1) * 50;
+        int[] manaCosts = {100, 200, 300};
+        return manaCosts[Math.min(spellLevel - 1, manaCosts.length - 1)];
     }
 
     @Override
