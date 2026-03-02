7694df3ded3d191f34e35c7f97c7e9092c585135
Xiaoyu_2009 <3551371733@qq.com>
2026-03-03 00:21:09 +0800

    咕咕嘎嘎整理一下4

--- src/main/java/miku/united_as_one/genesis/common/items/weapon/sword/DivineMetalSword.java
+++ src/main/java/miku/united_as_one/genesis/common/items/weapon/sword/DivineMetalSword.java
@@ -1,4 +1,4 @@
-package miku.united_as_one.genesis.common.items.hoe;
+package miku.united_as_one.genesis.common.items.weapon.sword;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.*;
@@ -7,13 +7,13 @@ import org.jetbrains.annotations.*;
 
 import java.util.List;
 
-public class DivineMetalHoe extends HoeItem {
-    public DivineMetalHoe(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
+public class DivineMetalSword extends SwordItem {
+    public DivineMetalSword(Tier tier, int attackDamage, float attackSpeed, Properties properties) {
         super(tier, attackDamage, attackSpeed, properties);
     }
 
     public void appendHoverText(@NotNull ItemStack pStack, @Nullable Level pLevel, @NotNull List<Component> pTooltipComponents, @NotNull TooltipFlag pIsAdvanced) {
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
-        pTooltipComponents.add(Component.translatable("tooltip.iron_spells_genesis.divine_metal.description_4"));
+        pTooltipComponents.add(Component.translatable("tooltip.iron_spells_genesis.divine_metal.description_1"));
     }
 }
