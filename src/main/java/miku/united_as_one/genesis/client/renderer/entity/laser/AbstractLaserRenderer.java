12cdca2383a26dd99b004b01cba52e5973268791
Xiaoyu_2009 <3551371733@qq.com>
2026-03-05 22:55:18 +0800

    抽象基类

---
+++ src/main/java/miku/united_as_one/genesis/client/renderer/entity/laser/AbstractLaserRenderer.java
@@ -0,0 +1,147 @@
+package miku.united_as_one.genesis.client.renderer.entity.laser;
+
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Axis;
+import miku.united_as_one.genesis.common.entity.laser.AbstractLaserEntity;
+import net.minecraft.client.*;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.entity.*;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.*;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+
+@OnlyIn(Dist.CLIENT)
+public abstract class AbstractLaserRenderer<T extends AbstractLaserEntity> extends EntityRenderer<T> {
+    private boolean clearerView = false;
+
+    public AbstractLaserRenderer(EntityRendererProvider.Context mgr) {
+        super(mgr);
+    }
+
+    @Override
+    public @NotNull ResourceLocation getTextureLocation(@NotNull T entity) {
+        return getLaserTexture();
+    }
+
+    protected abstract ResourceLocation getLaserTexture();
+
+    @Override
+    public void render(T laser, float entityYaw, float delta, @NotNull PoseStack matrixStackIn, @NotNull MultiBufferSource bufferIn, int packedLightIn) {
+        this.clearerView = (laser.caster instanceof Player && (Minecraft.getInstance()).player == laser.caster && (Minecraft.getInstance()).options.getCameraType() == CameraType.FIRST_PERSON);
+        
+        double posX = laser.xo + (laser.getX() - laser.xo) * delta;
+        double posY = laser.yo + (laser.getY() - laser.yo) * delta;
+        double posZ = laser.zo + (laser.getZ() - laser.zo) * delta;
+        float yaw = laser.prevYaw + (laser.renderYaw - laser.prevYaw) * delta;
+        float pitch = laser.prevPitch + (laser.renderPitch - laser.prevPitch) * delta;
+        
+        int frame = Mth.floor(((laser.appearTimer - 1) + delta) * 2);
+        if (frame < 0) frame = 6;
+        
+        RenderType.CompositeState rendertype$state = RenderType.CompositeState.builder()
+                .setTextureState(new RenderStateShard.TextureStateShard(getTextureLocation(laser), false, false))
+                .setShaderState(RenderStateShard.RENDERTYPE_EYES_SHADER)
+                .setTransparencyState(RenderType.TRANSLUCENT_TRANSPARENCY)
+                .setCullState(RenderStateShard.NO_CULL)
+                .setOverlayState(RenderStateShard.OVERLAY)
+                .setWriteMaskState(RenderStateShard.COLOR_WRITE)
+                .createCompositeState(false);
+        
+        VertexConsumer ivertexbuilder = bufferIn.getBuffer(RenderType.create("glow_beam", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, true, true, rendertype$state));
+        
+        if (laser.getRenderStart()) renderStart(frame, matrixStackIn, ivertexbuilder, packedLightIn);
+        renderBeam(laser.getLaserLength(), 57 * yaw, 57 * pitch, frame, matrixStackIn, ivertexbuilder, packedLightIn);
+        if (laser.getRenderEnd()) {
+            matrixStackIn.pushPose();
+            double endX = posX + laser.getLaserLength() * Math.cos(Math.toRadians(57 * yaw)) * Math.cos(Math.toRadians(57 * pitch));
+            double endZ = posZ + laser.getLaserLength() * Math.sin(Math.toRadians(57 * yaw)) * Math.cos(Math.toRadians(57 * pitch));
+            double endY = posY + laser.getLaserLength() * Math.sin(Math.toRadians(57 * pitch));
+            matrixStackIn.translate(endX - posX, endY - posY, endZ - posZ);
+            renderEnd(frame, laser.blockSide, matrixStackIn, ivertexbuilder, packedLightIn);
+            matrixStackIn.popPose();
+        }
+    }
+
+    protected void renderFlatQuad(int frame, PoseStack matrixStackIn, VertexConsumer builder, int packedLightIn) {
+        float minU = 0 + 0.0625f * frame;
+        float minV = 0;
+        float maxU = minU + 0.0625f;
+        float maxV = minV + 0.5f;
+        PoseStack.Pose matrixstack$entry = matrixStackIn.last();
+        Matrix4f matrix4f = matrixstack$entry.pose();
+        Matrix3f matrix3f = matrixstack$entry.normal();
+        drawVertex(matrix4f, matrix3f, builder, -1.3f, -1.3f, 0, minU, minV, 1, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, -1.3f, 1.3f, 0, minU, maxV, 1, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, 1.3f, 1.3f, 0, maxU, maxV, 1, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, 1.3f, -1.3f, 0, maxU, minV, 1, packedLightIn);
+    }
+
+    protected void renderStart(int frame, PoseStack matrixStackIn, VertexConsumer builder, int packedLightIn) {
+        if (this.clearerView) return;
+        matrixStackIn.pushPose();
+        Quaternionf quat = this.entityRenderDispatcher.cameraOrientation();
+        matrixStackIn.mulPose(quat);
+        renderFlatQuad(frame, matrixStackIn, builder, packedLightIn);
+        matrixStackIn.popPose();
+    }
+
+    protected void renderEnd(int frame, Direction side, PoseStack matrixStackIn, VertexConsumer builder, int packedLightIn) {
+        matrixStackIn.pushPose();
+        Quaternionf quat = this.entityRenderDispatcher.cameraOrientation();
+        matrixStackIn.mulPose(quat);
+        renderFlatQuad(frame, matrixStackIn, builder, packedLightIn);
+        matrixStackIn.popPose();
+        if (side == null) return;
+        matrixStackIn.pushPose();
+        Quaternionf sideQuat = side.getRotation();
+        sideQuat.mul(Axis.XP.rotationDegrees(90));
+        matrixStackIn.mulPose(sideQuat);
+        matrixStackIn.translate(0, 0, -0.01f);
+        renderFlatQuad(frame, matrixStackIn, builder, packedLightIn);
+        matrixStackIn.popPose();
+    }
+
+    protected void drawBeam(float length, int frame, PoseStack matrixStackIn, VertexConsumer builder, int packedLightIn) {
+        float minU = 0f;
+        float minV = 0.5f + 0.03125f * frame;
+        float maxU = minU + 0.078125f;
+        float maxV = minV + 0.03125f;
+        PoseStack.Pose matrixstack$entry = matrixStackIn.last();
+        Matrix4f matrix4f = matrixstack$entry.pose();
+        Matrix3f matrix3f = matrixstack$entry.normal();
+        float offset = this.clearerView ? -1 : 0;
+        drawVertex(matrix4f, matrix3f, builder, -1, offset, 0.0F, minU, minV, 1.0F, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, -1, length, 0.0F, minU, maxV, 1.0F, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, 1, length, 0.0F, maxU, maxV, 1.0F, packedLightIn);
+        drawVertex(matrix4f, matrix3f, builder, 1, offset, 0.0F, maxU, minV, 1.0F, packedLightIn);
+    }
+
+    protected void renderBeam(float length, float yaw, float pitch, int frame, PoseStack matrixStackIn, VertexConsumer builder, int packedLightIn) {
+        matrixStackIn.pushPose();
+        matrixStackIn.mulPose(Axis.XP.rotationDegrees(90));
+        matrixStackIn.mulPose(Axis.ZP.rotationDegrees(yaw - 90));
+        matrixStackIn.mulPose(Axis.XP.rotationDegrees(-pitch));
+        matrixStackIn.pushPose();
+        if (!this.clearerView) matrixStackIn.mulPose(Axis.YP.rotationDegrees((Minecraft.getInstance()).gameRenderer.getMainCamera().getXRot() + 90));
+        drawBeam(length, frame, matrixStackIn, builder, packedLightIn);
+        matrixStackIn.popPose();
+        if (!this.clearerView) {
+            matrixStackIn.pushPose();
+            matrixStackIn.mulPose(Axis.YP.rotationDegrees(-(Minecraft.getInstance()).gameRenderer.getMainCamera().getXRot() - 90));
+            drawBeam(length, frame, matrixStackIn, builder, packedLightIn);
+            matrixStackIn.popPose();
+        }
+        matrixStackIn.popPose();
+    }
+
+    public void drawVertex(Matrix4f matrix, Matrix3f normals, VertexConsumer vertexBuilder, float offsetX, float offsetY, float offsetZ, float textureX, float textureY, float alpha, int packedLightIn) {
+        vertexBuilder.vertex(matrix, offsetX, offsetY, offsetZ).color(1, 1, 1, alpha).uv(textureX, textureY).overlayCoords(OverlayTexture.NO_OVERLAY).uv2(packedLightIn).normal(normals, 0, 1, 0).endVertex();
+    }
+}
