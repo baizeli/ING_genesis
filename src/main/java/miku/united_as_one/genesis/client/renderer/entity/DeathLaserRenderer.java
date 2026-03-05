be91cb9beae22fc5f2b0f84407f83e13fff46eea
Xiaoyu_2009 <3551371733@qq.com>
2026-03-05 22:05:52 +0800

    贝利亚大战奥特之母被奥特之父干死

--- src/main/java/miku/united_as_one/genesis/client/renderer/entity/DeathLaserRenderer.java
+++ src/main/java/miku/united_as_one/genesis/client/renderer/entity/DeathLaserRenderer.java
@@ -60,7 +60,10 @@ public class DeathLaserRenderer extends EntityRenderer<DeathLaserEntity> {
         renderBeam(solarBeam.getLaserLength(), 57 * yaw, 57 * pitch, frame, matrixStackIn, ivertexbuilder, packedLightIn);
         if (solarBeam.getRenderEnd()) {
             matrixStackIn.pushPose();
-            matrixStackIn.translate(collidePosX - posX, collidePosY - posY, collidePosZ - posZ);
+            double endX = posX + solarBeam.getLaserLength() * Math.cos(yaw) * Math.cos(pitch);
+            double endZ = posZ + solarBeam.getLaserLength() * Math.sin(yaw) * Math.cos(pitch);
+            double endY = posY + solarBeam.getLaserLength() * Math.sin(pitch);
+            matrixStackIn.translate(endX - posX, endY - posY, endZ - posZ);
             renderEnd(frame, solarBeam.blockSide, matrixStackIn, ivertexbuilder, packedLightIn);
             matrixStackIn.popPose();
         }
