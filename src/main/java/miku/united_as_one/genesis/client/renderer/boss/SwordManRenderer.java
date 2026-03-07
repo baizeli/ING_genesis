53d6b072468f588d88aa9be895d7c7bd3fd2b129
NineAbyss <woxingzhoue@outlook.com>
2026-03-07 15:55:30 +0800

    守门人基础

---
+++ src/main/java/miku/united_as_one/genesis/client/renderer/boss/SwordManRenderer.java
@@ -0,0 +1,7 @@
+
+package miku.united_as_one.genesis.client.renderer.boss;
+
+import miku.united_as_one.genesis.common.entity.boss.SwordMan;
+
+public class SwordManRenderer<E extends SwordMan> {
+}
