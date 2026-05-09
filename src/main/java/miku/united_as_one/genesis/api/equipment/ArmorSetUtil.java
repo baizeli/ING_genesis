94fd6bd4e1cae1b34d43e07e89709d6ae8d7826a
Xiaoyu_2009 <3551371733@qq.com>
2026-05-09 23:06:37 +0800

    添加奥术水晶套相关....

--- src/main/java/miku/united_as_one/genesis/api/equipment/ArmorSetUtil.java
+++ src/main/java/miku/united_as_one/genesis/api/equipment/ArmorSetUtil.java
@@ -1,7 +1,6 @@
 package miku.united_as_one.genesis.api.equipment;
 
-import miku.united_as_one.genesis.contents.items.armor.CelestialSourceSpellArmor;
-import miku.united_as_one.genesis.contents.items.armor.VioletZenithArmor;
+import miku.united_as_one.genesis.contents.items.armor.*;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -14,20 +13,6 @@ public class ArmorSetUtil {
                 ItemStack stack = entity.getItemBySlot(slot);
 
                 if (!(stack.getItem() instanceof CelestialSourceSpellArmor)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-    
-    public static boolean hasFullVioletZenithSet(LivingEntity entity) {
-        for (EquipmentSlot slot : EquipmentSlot.values()) {
-            if (slot.getType() == EquipmentSlot.Type.ARMOR) {
-                ItemStack stack = entity.getItemBySlot(slot);
-
-                if (!(stack.getItem() instanceof VioletZenithArmor)) {
                     return false;
                 }
             }
